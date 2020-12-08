79cf4b2276c7fc1712be3a906f6f53ef81cd9694
Max Kratzok <50670927+MKratzok@users.noreply.github.com>
2020-12-08 13:02:45 -0500

    main now has manual mode!

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,7 @@
 import robot
 import sys
 from time import sleep
+import sensors
 
 
 def run_auto(r):
@@ -8,7 +9,28 @@ def run_auto(r):
     return
 
 
-def run_manual(r):
+def menu():
+    print('Command Options:\n'
+          '\t[W]alk\n'
+          '\t[T]urn 12 steps\n'
+          '\t[M]anual turn\n'
+          '\t[C]limb\n'
+          '\t[R]eset\n'
+          '\t[E]xtend\n'
+          '\t[S]ense range\n'
+          '\t[L]ight level\n'
+          '\t[P]icture\n'
+          '\t[Q]uit\n')
+    c = input().lower()
+
+    if c in ['w', 't', 'c', 'r', 'e', 's', 'l', 'p', 'q', 'm']:
+        return c
+    else:
+        print('Command not found.')
+        return menu()
+
+
+def run_manual(r, s):
     """
     run_manual(r) - Gives user control of robot until they quit. Control with WASD keys
     :param r: a robot class
@@ -17,15 +39,36 @@ def run_manual(r):
     print("   ___________________________________")
     print("  /                                  /")
     print(" /           MANUAL MODE            /")
-    print("/__________________________________/")
+    print("/__________________________________/\n\n")
 
-    sleep(1)
+    c = ''
 
-    c = ""
+    while c != "q":
+        c = menu()
 
-    while c.lower() != "q":
-        c = input().lower()
-        # TODO
+        if c == 'w':
+            steps = input('\t\tHow many steps?')
+            r.walk(steps)
+        elif c == 't':
+            r.turning(12)
+        elif c == 'm':
+            steps = input('\t\tHow many steps?')
+            r.turning(steps)
+        elif c == 'r':
+            r.reset()
+        elif c == 'e':
+            r.extend()
+        elif c == 's':
+            print(('Range {}mm'.format(s.range).ljust(20, '-')).rjust(30,'-'))
+        elif c == 'l':
+            print(('Light {} lux'.format(11).ljust(21, '-')).rjust(30,'-'))
+        elif c == 'p':
+            s.take_photo()
+        elif c == 'q':
+            return
+        else:
+            print('This should not happen. Please contact MAX and tell him that \"' + c + '\" messed up the cmd seq.')
+            exit(-69)
 
 
 if __name__ == "__main__":
@@ -39,12 +82,14 @@ if __name__ == "__main__":
     elif sys.argv[1] in ['--manual', '-m']:
         auto = False
     else:
+        auto = False
         print('usage: main.py [-a or --auto] or [-m --manual]')
         quit()
 
     r = robot.Robot()
+    s = sensors.Sensors()
 
     if auto:
         run_auto(r)
     else:
-        run_manual(r)
+        run_manual(r, s)
